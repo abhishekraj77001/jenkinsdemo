bb835eb5150251f800e2ac6eb2cee25bc7c5c1da
Abhishek Raj <abhishekraj77001@gmail.com>
2022-10-08 18:43:42 +0530

    jenkinsdemo project

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/demo/")
+public class Controller {
+
+	@GetMapping("health")
+	public String healthCheck()
+	{
+		return "Application is up and running...";
+	}
+}
+
